75c7b467ba5ed3e69cab9ae8403aefe9985abced
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-07-14 22:14:28 -0300

    feat: homolog status: ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDto.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDto.java
@@ -24,7 +24,7 @@ public class GameMinDto {
     public GameMinDto(GameMinProjection projection) {
         id = projection.getId();
         title = projection.getTitle();
-        year = projection.getYear();
+        year = projection.getGameYear();
         imgUrl = projection.getImgUrl();
         shortDescription = projection.getShortDescription();
     }
@@ -37,7 +37,7 @@ public class GameMinDto {
         return title;
     }
 
-    public Integer getYear() {
+    public Integer getGameYear() {
         return year;
     }
 
